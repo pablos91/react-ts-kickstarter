d81cefaa472c1ca1523bb15f0d14e435c811b534
pablito <pawel@maple.com.pl>
2018-01-13 19:36:24 +0100

    minor fix for loader

--- webpack.config.js
+++ webpack.config.js
@@ -78,6 +78,9 @@ module.exports = {
                 test: require.resolve('jquery'),
                 use: [{
                     loader: 'expose-loader',
+                    options: 'jQuery'
+                },{
+                    loader: 'expose-loader',
                     options: '$'
                 }]
             }
