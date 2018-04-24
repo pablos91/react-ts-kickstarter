1aa4d00c993e6850af7dba319b10c4b8818c3f16
Paweł Niewiadomski <pawel.niewiadomski@loyaltypoint.pl>
2018-04-24 11:27:53 +0200

    forcing require function usage

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
+import MasterPage from './pages/master';
+
 import 'semantic-ui-css/semantic.min.css';
 import './scss/main.scss';
-import {BrowserRouter} from 'react-router-dom';
-
-import MasterPage from './pages/master';
 
 ReactDOM.render(
   <BrowserRouter>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,22 @@ import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 class IndexPage extends React.Component {
+
+  componentDidMount(){
+    alert(1);
+  }
+
   render() {
 
     return (
-        <main>Strona startowa</main> 
+        <main>
+          
+          Strona startowa
+
+          <img src={require('../images/logo.png')} />
+
+        </main> 
+        
     );
   }
 }
