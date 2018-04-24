b2139368ac752bf1af40b69844003c7fd1eb8e3a
Paweł Niewiadomski <pawel.niewiadomski@loyaltypoint.pl>
2018-04-24 10:48:16 +0200

    added chrome debugger

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import './scss/main.scss';
 import {BrowserRouter} from 'react-router-dom';
 
 import MasterPage from './pages/master';
