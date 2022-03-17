5c6c8cf26b10a947316a0cc0aff789cdf4463419
Pablito <1850835+pablos91@users.noreply.github.com>
2022-03-17 19:49:31 +0100

    added dynamically generated fields for react-hook-form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as React from 'react';
-import { useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import './login.scss';
 
@@ -12,24 +12,53 @@ const validationSchema = yup.object().shape({
     password: yup
         .string()
         .required(),
-    // consents: yup.array().of(
-    //     yup.object().shape({
-    //         isRequired: yup.boolean(),
-    //         consentValue: yup
-    //             .boolean()
-    //             .when("isRequired", {
-    //                 is: true,
-    //                 then: yup.boolean().isTrue("Consent required")
-    //             }),
-    //     })
-    // )
+    consents: yup.array().of(
+        yup.object().shape({
+            isRequired: yup.boolean(),
+            consentValue: yup
+                .boolean()
+                .when("isRequired", {
+                    is: true,
+                    then: yup.boolean().isTrue("Consent required")
+                }),
+        })
+    )
 });
 
 export const LoginPage = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    const { register, control, handleSubmit, watch, setError, formState: { errors } } = useForm({
         resolver: yupResolver(validationSchema)
     });
-    const onSubmit = data => console.log(data);
+    const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+        control, // control props comes from useForm (optional: if you are using FormContext)
+        name: "consents", // unique name for your Field Array
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+        setError("consents.1.consentValue", { message: "Error from API call!" })
+    };
+
+    const consents = [
+        {
+            consentValue: false,
+            isRequired: true,
+            consentText: "Consent A",
+            consentId: 1
+        },
+        {
+            consentValue: false,
+            isRequired: true,
+            consentText: "Consent b",
+            consentId: 2
+        },
+    ]
+
+    React.useEffect(() => {
+        consents.map((elem) => {
+            append(elem);
+        })
+    }, [])
 
     console.log(watch("email"));
 
@@ -41,6 +70,18 @@ export const LoginPage = () => {
 
                 <input key="password" {...register("password")} />
                 {errors.password && <span>{errors.password.message}</span>}
+
+                {fields.map((field: Record<"id" | "consentText" | "isRequired", any>, index) => (
+                    <label key={field.id}>
+                        <input
+                            type="checkbox"
+                            {...register(`consents.${index}.consentValue`)}
+                        />
+                        {field.consentText}{field.isRequired && "*"}
+                        {errors.consents?.[index] && <span>{errors.consents?.[index].consentValue.message}</span>}
+                    </label>
+
+                ))}
 
                 <input type="submit" />
             </form>
