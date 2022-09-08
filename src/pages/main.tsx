e79be2a016d06491e6ae4b206a4d57304dcd3cea
Paweł Niewiadomski <pawel.niewiadomski@loyaltypoint.pl>
2022-09-08 16:33:00 +0200

    added language switcher

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,14 +1,25 @@
 import { useSampleContext } from 'contexts/sampleContext';
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 import './main.scss';
 
 export const MainPage = () => {
+  const { t, i18n } = useTranslation();
+
+
+  const changeLanguage = lng => {
+    //@ts-ignore
+    i18n.changeLanguage(lng);
+  };
+
 
   const [{ }, dispatch] = useSampleContext();
 
   return (
     <div className='main-page-container'>
-      <div className="greetings-container"><h1>Welcome to MainPage!</h1></div>
+      <div className="greetings-container"><h1>{t("welcome")}</h1></div>
+      <button onClick={() => changeLanguage("pl")}>PL</button>
+      <button onClick={() => changeLanguage("en")}>EN</button>
       <div className="additional-info-container">
         <button onClick={() => dispatch({ type: "changeColor" })}>Toggle color!!!!!!!!!!</button>
         <p>REACT_APP_API_URL: {process.env.REACT_APP_API_URL}</p>
